a21fca5f597698f78c6efa09c38c55685cc79dd2
Dante O. Cuales <danteocualesjr@gmail.com>
2022-01-05 16:14:38 +0800

    Added main() function

---
+++ index.js
@@ -0,0 +1,8 @@
+async function main() {
+    const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
+    const web3 = createAlchemyWeb3("https://eth-ropsten.alchemyapi.io/v2/21phU3FCMDFObc_IL49DIezWerwmj0HJ");
+    const blockNumber = await web3.eth.getBlockNumber();
+    console.log("The latest block number is " + blockNumber);
+}
+
+main();
